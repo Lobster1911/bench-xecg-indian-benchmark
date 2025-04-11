10dad499bd5ac7d7a4dfcdc8d7a848057335260d
luna97 <rikilnl@gmail.com>
2025-04-11 10:27:01 +0200

    wip

--- pretrain_slurm.sh
+++ pretrain_slurm.sh
@@ -1,17 +1,20 @@
 #!/bin/bash -l
 
 #SBATCH --job-name=pretrain_xlstm
+#SBATCH --partition=h100
 #SBATCH --gpus=1
 #SBATCH --ntasks=1
 #SBATCH --cpus-per-task=64
 #SBATCH --mem-per-cpu=1G
-#SBATCH --time=3-00:00:00
+#SBATCH --time=2-00:00:00
 #SBATCH -o ./logs/slurm_output_%j_%x.out # STDOUT
+
+echo "Visible GPUs: "
+echo $CUDA_VISIBLE_DEVICES
 
 source /home/$USER/.bashrc
 conda init
 conda activate xlstm_pretrained
-
 
 # run script from above
 srun python3 -u pretrain.py
