c6a6d3aacd4c760fcdaab5bc4a7377e4c96cf0d9
luna97 <rikilnl@gmail.com>
2025-05-14 15:14:12 +0200

    lol

---
+++ slumr_pretrain_l40s.sh
@@ -0,0 +1,19 @@
+#!/bin/bash -l
+#SBATCH --job-name=ptb_xl
+#SBATCH --partition=l40s
+#SBATCH --gpus=1
+#SBATCH --ntasks=1
+#SBATCH --cpus-per-task=128
+#SBATCH --mem-per-cpu=1G
+#SBATCH --time=7-00:00:00
+#SBATCH -o ./logs/slurm_output_%j_%x.out # STDOUT
+
+echo "Visible GPUs: "
+echo $CUDA_VISIBLE_DEVICES
+
+#source /home/$USER/.bashrc
+#conda init
+#conda activate xlstm_pretrained
+
+# run script from above
+srun ~/.conda/envs/xlstm_pretrained/bin/python -u pretrain.py
