ac9278bc4950a8abade625a058b7be706ea69b36
luna97 <rikilnl@gmail.com>
2025-06-18 15:39:28 +0200

    lol

---
+++ slumr_train_mit_bih.sh
@@ -0,0 +1,19 @@
+#!/bin/bash -l
+#SBATCH --job-name=mit_bih
+#SBATCH --partition=l40s
+#SBATCH --gpus=1
+#SBATCH --ntasks=1
+#SBATCH --cpus-per-task=64
+#SBATCH --mem-per-cpu=512M
+#SBATCH --time=7-00:00:00
+#SBATCH -o ./logs/slurm_output_%j_%x.out # STDOUT
+
+echo "Visible GPUs: "
+echo $CUDA_VISIBLE_DEVICES
+
+#source /home/$USER/.bashrc
+#conda init
+#conda activate xlstm_pretrained
+
+# run script from above
+srun ~/.conda/envs/xlstm_pretrained/bin/python -u train_mit_bih.py
