e11066566249fa8a763ff68b1334b106a4aea2d3
luna97 <rikilnl@gmail.com>
2025-06-23 10:57:51 +0200

    wip metrics

--- trainers/mit_bih_trainer.py
+++ trainers/mit_bih_trainer.py
@@ -57,20 +57,20 @@ class TrainingMIT_BIH(CommonTrainerDownstream):
         self.train_f1 = self.train_f1.to(preds.device)
         self.train_f1(preds, targets)
 
-        self.log('train_loss', loss_cls.detach().item(), prog_bar=True, batch_size=self.batch_size)
+        self.log('train_loss', loss_cls.detach().item(), prog_bar=True)
 
-        self.log('train_r_peak_loss', loss_r_peak_pos.detach().item(), prog_bar=True, batch_size=self.batch_size)
-        self.log('train_rec_r_peak', self.train_acc_r_peak, prog_bar=True, batch_size=self.batch_size)
-        self.log('train_f1_r_peak', self.train_f1_r_peak, prog_bar=True, batch_size=self.batch_size)
+        self.log('train_r_peak_loss', loss_r_peak_pos.detach().item(), prog_bar=True)
+        self.log('train_rec_r_peak', self.train_acc_r_peak, prog_bar=True)
+        self.log('train_f1_r_peak', self.train_f1_r_peak, prog_bar=True)
 
-        self.log('train_acc', self.train_acc, prog_bar=True, batch_size=self.batch_size)
-        self.log('train_f1', self.train_f1, prog_bar=True, batch_size=self.batch_size)
+        self.log('train_acc', self.train_acc, prog_bar=True)
+        self.log('train_f1', self.train_f1, prog_bar=True)
 
         # auroc
         # self.train_auroc = self.train_auroc.cpu()
         self.train_auroc = self.train_auroc.to(logits.device)
         self.train_auroc(logits, targets)
-        self.log("train_auroc", self.train_auroc, batch_size=self.batch_size)
+        self.log("train_auroc", self.train_auroc)
 
         return loss_cls + loss_r_peak_pos * self.r_peaks_lambda
     
@@ -89,49 +89,49 @@ class TrainingMIT_BIH(CommonTrainerDownstream):
         self.valid_f1 = self.valid_f1.to(preds.device)
         self.valid_f1(preds, targets)
 
-        self.log('val_loss', loss_cls.detach().item(), prog_bar=True, batch_size=self.batch_size)
+        self.log('val_loss', loss_cls.detach().item(), prog_bar=True)
 
-        self.log('val_r_peak_loss', loss_r_peak_pos.detach().item(), prog_bar=True, batch_size=self.batch_size)
-        self.log('val_rec_r_peak', self.valid_acc_r_peak, prog_bar=True, batch_size=self.batch_size)
-        self.log('val_f1_r_peak', self.valid_f1_r_peak, prog_bar=True, batch_size=self.batch_size)
+        self.log('val_r_peak_loss', loss_r_peak_pos.detach().item(), prog_bar=True)
+        self.log('val_rec_r_peak', self.valid_acc_r_peak, prog_bar=True)
+        self.log('val_f1_r_peak', self.valid_f1_r_peak, prog_bar=True)
 
-        self.log('val_acc', self.valid_acc, prog_bar=True, batch_size=self.batch_size)
-        self.log('val_f1', self.valid_f1, prog_bar=True, batch_size=self.batch_size)
+        self.log('val_acc', self.valid_acc, prog_bar=True)
+        self.log('val_f1', self.valid_f1, prog_bar=True)
 
         # specificity
         self.val_spec = self.val_spec.to(preds.device)
         self.val_spec(preds, targets)
-        self.log('val_specificity/N', self.val_spec[0], prog_bar=False, batch_size=self.batch_size, metric_attribute='val_spec')
-        self.log('val_specificity/S', self.val_spec[1], prog_bar=False, batch_size=self.batch_size, metric_attribute='val_spec')
-        self.log('val_specificity/V', self.val_spec[2], prog_bar=False, batch_size=self.batch_size, metric_attribute='val_spec')
+        self.log('val_specificity/N', self.val_spec[0], prog_bar=False, metric_attribute='val_spec')
+        self.log('val_specificity/S', self.val_spec[1], prog_bar=False, metric_attribute='val_spec')
+        self.log('val_specificity/V', self.val_spec[2], prog_bar=False, metric_attribute='val_spec')
         if self.num_classes == 5:
-            self.log('val_specificity/F', self.val_spec[3], prog_bar=False, batch_size=self.batch_size, metric_attribute='val_spec')
-            self.log('val_specificity/Q', self.val_spec[4], prog_bar=False, batch_size=self.batch_size, metric_attribute='val_spec')
+            self.log('val_specificity/F', self.val_spec[3], prog_bar=False, metric_attribute='val_spec')
+            self.log('val_specificity/Q', self.val_spec[4], prog_bar=False, metric_attribute='val_spec')
 
         # sensitivity
         self.val_recall = self.val_recall.to(preds.device)
         self.val_recall(preds, targets)
-        self.log('val_sensitivity/N', self.val_recall[0], prog_bar=False, batch_size=self.batch_size, metric_attribute='val_recall')
-        self.log('val_sensitivity/S', self.val_recall[1], prog_bar=False, batch_size=self.batch_size, metric_attribute='val_recall')
-        self.log('val_sensitivity/V', self.val_recall[2], prog_bar=False, batch_size=self.batch_size, metric_attribute='val_recall')
+        self.log('val_sensitivity/N', self.val_recall[0], prog_bar=False, metric_attribute='val_recall')
+        self.log('val_sensitivity/S', self.val_recall[1], prog_bar=False, metric_attribute='val_recall')
+        self.log('val_sensitivity/V', self.val_recall[2], prog_bar=False, metric_attribute='val_recall')
         if self.num_classes == 5:
-            self.log('val_sensitivity/F', self.val_recall[3], prog_bar=False, batch_size=self.batch_size, metric_attribute='val_recall')
-            self.log('val_sensitivity/Q', self.val_recall[4], prog_bar=False, batch_size=self.batch_size, metric_attribute='val_recall')
+            self.log('val_sensitivity/F', self.val_recall[3], prog_bar=False, metric_attribute='val_recall')
+            self.log('val_sensitivity/Q', self.val_recall[4], prog_bar=False, metric_attribute='val_recall')
 
         # ppv
         self.val_precision = self.val_precision.to(preds.device)
         self.val_precision(preds, targets)
-        self.log('val_ppv/N', self.val_precision[0], prog_bar=False, batch_size=self.batch_size, metric_attribute='val_precision')
-        self.log('val_ppv/S', self.val_precision[1], prog_bar=False, batch_size=self.batch_size, metric_attribute='val_precision')
-        self.log('val_ppv/V', self.val_precision[2], prog_bar=False, batch_size=self.batch_size, metric_attribute='val_precision')
+        self.log('val_ppv/N', self.val_precision[0], prog_bar=False, metric_attribute='val_precision')
+        self.log('val_ppv/S', self.val_precision[1], prog_bar=False, metric_attribute='val_precision')
+        self.log('val_ppv/V', self.val_precision[2], prog_bar=False, metric_attribute='val_precision')
         if self.num_classes == 5:
-            self.log('val_ppv/F', self.val_precision[3], prog_bar=False, batch_size=self.batch_size, metric_attribute='val_precision')
-            self.log('val_ppv/Q', self.val_precision[4], prog_bar=False, batch_size=self.batch_size, metric_attribute='val_precision')
+            self.log('val_ppv/F', self.val_precision[3], prog_bar=False, metric_attribute='val_precision')
+            self.log('val_ppv/Q', self.val_precision[4], prog_bar=False, metric_attribute='val_precision')
 
         # auroc
         self.valid_auroc = self.valid_auroc.to(logits.device)
         self.valid_auroc(logits, targets)
-        self.log('val_auroc', self.valid_auroc, prog_bar=True, batch_size=self.batch_size)
+        self.log('val_auroc', self.valid_auroc, prog_bar=True)
 
         return loss_cls + loss_r_peak_pos * self.r_peaks_lambda
             
@@ -153,67 +153,76 @@ class TrainingMIT_BIH(CommonTrainerDownstream):
         self.test_f1 = self.test_f1.to(preds.device)
         self.test_f1(preds, targets)
 
-        self.log("test_loss", loss_cls.detach().item(), batch_size=self.batch_size)
+        self.log("test_loss", loss_cls.detach().item())
 
-        self.log("test_r_peak_loss", loss_r_peak_pos.detach().item(), batch_size=self.batch_size)
-        self.log("test_rec_r_peak", self.test_acc_r_peak, batch_size=self.batch_size)
-        self.log("test_f1_r_peak", self.test_f1_r_peak, batch_size=self.batch_size)
+        self.log("test_r_peak_loss", loss_r_peak_pos.detach().item())
+        self.log("test_rec_r_peak", self.test_acc_r_peak)
+        self.log("test_f1_r_peak", self.test_f1_r_peak)
 
-        self.log("test_acc", self.test_acc, batch_size=self.batch_size)
+        self.log("test_acc", self.test_acc)
 
         # accuracy
-        self.log("test_acc/N", self.test_acc_no_avg[0], batch_size=self.batch_size, metric_attribute='test_acc')
-        self.log("test_acc/S", self.test_acc_no_avg[1], batch_size=self.batch_size, metric_attribute='test_acc')
-        self.log("test_acc/V", self.test_acc_no_avg[2], batch_size=self.batch_size, metric_attribute='test_acc')
+        self.log("test_acc/N", self.test_acc_no_avg[0], metric_attribute='test_acc')
+        self.log("test_acc/S", self.test_acc_no_avg[1], metric_attribute='test_acc')
+        self.log("test_acc/V", self.test_acc_no_avg[2], metric_attribute='test_acc')
         if self.num_classes == 5:
-            self.log("test_acc/F", self.test_acc_no_avg[3], batch_size=self.batch_size, metric_attribute='test_acc')
-            self.log("test_acc/Q", self.test_acc_no_avg[4], batch_size=self.batch_size, metric_attribute='test_acc')
+            self.log("test_acc/F", self.test_acc_no_avg[3], metric_attribute='test_acc')
+            self.log("test_acc/Q", self.test_acc_no_avg[4], metric_attribute='test_acc')
 
         # f1
-        self.log("test_f1/N", self.test_f1[0], batch_size=self.batch_size, metric_attribute='test_f1')
-        self.log("test_f1/S", self.test_f1[1], batch_size=self.batch_size, metric_attribute='test_f1')
-        self.log("test_f1/V", self.test_f1[2], batch_size=self.batch_size, metric_attribute='test_f1')
+        self.log("test_f1/N", self.test_f1[0], metric_attribute='test_f1')
+        self.log("test_f1/S", self.test_f1[1], metric_attribute='test_f1')
+        self.log("test_f1/V", self.test_f1[2], metric_attribute='test_f1')
         if self.num_classes == 5:
-            self.log("test_f1/F", self.test_f1[3], batch_size=self.batch_size, metric_attribute='test_f1')
-            self.log("test_f1/Q", self.test_f1[4], batch_size=self.batch_size, metric_attribute='test_f1')
-            self.log("test_f1/mean", (self.test_f1[0] + self.test_f1[1] + self.test_f1[2] + self.test_f1[3] + self.test_f1[4]) / 5, batch_size=self.batch_size, metric_attribute='test_f1')
+            self.log("test_f1/F", self.test_f1[3], metric_attribute='test_f1')
+            self.log("test_f1/Q", self.test_f1[4], metric_attribute='test_f1')
+            self.log("test_f1/mean", (self.test_f1[0] + self.test_f1[1] + self.test_f1[2] + self.test_f1[3] + self.test_f1[4]) / 5, metric_attribute='test_f1')
         else:
-            self.log("test_f1/mean", (self.test_f1[0] + self.test_f1[1] + self.test_f1[2]) / 3, batch_size=self.batch_size, metric_attribute='test_f1')
+            self.log("test_f1/mean", (self.test_f1[0] + self.test_f1[1] + self.test_f1[2]) / 3, metric_attribute='test_f1')
 
         # specificity
         self.test_spec = self.test_spec.to(preds.device)
         self.test_spec(preds, targets)
-        self.log("test_specificity/N", self.test_spec[0], batch_size=self.batch_size, metric_attribute='test_spec')
-        self.log("test_specificity/S", self.test_spec[1], batch_size=self.batch_size, metric_attribute='test_spec')
-        self.log("test_specificity/V", self.test_spec[2], batch_size=self.batch_size, metric_attribute='test_spec')
+        self.log("test_specificity/N", self.test_spec[0], metric_attribute='test_spec')
+        self.log("test_specificity/S", self.test_spec[1], metric_attribute='test_spec')
+        self.log("test_specificity/V", self.test_spec[2], metric_attribute='test_spec')
         if self.num_classes == 5:
-            self.log("test_specificity/F", self.test_spec[3], batch_size=self.batch_size, metric_attribute='test_spec')
-            self.log("test_specificity/Q", self.test_spec[4], batch_size=self.batch_size, metric_attribute='test_spec')
+            self.log("test_specificity/F", self.test_spec[3], metric_attribute='test_spec')
+            self.log("test_specificity/Q", self.test_spec[4], metric_attribute='test_spec')
+            self.log("test_specificity/mean", (self.test_spec[0] + self.test_spec[1] + self.test_spec[2] + self.test_spec[3] + self.test_spec[4]) / 5, metric_attribute='test_spec')
+        else:
+            self.log("test_specificity/mean", (self.test_spec[0] + self.test_spec[1] + self.test_spec[2]) / 3, metric_attribute='test_spec')
 
         # sensitivity
         self.test_recall = self.test_recall.to(preds.device)
         self.test_recall(preds, targets)
-        self.log("test_sensitivity/N", self.test_recall[0], batch_size=self.batch_size, metric_attribute='test_recall')
-        self.log("test_sensitivity/S", self.test_recall[1], batch_size=self.batch_size, metric_attribute='test_recall')
-        self.log("test_sensitivity/V", self.test_recall[2], batch_size=self.batch_size, metric_attribute='test_recall')
+        self.log("test_sensitivity/N", self.test_recall[0], metric_attribute='test_recall')
+        self.log("test_sensitivity/S", self.test_recall[1], metric_attribute='test_recall')
+        self.log("test_sensitivity/V", self.test_recall[2], metric_attribute='test_recall')
         if self.num_classes == 5:
-            self.log("test_sensitivity/F", self.test_recall[3], batch_size=self.batch_size, metric_attribute='test_recall')
-            self.log("test_sensitivity/Q", self.test_recall[4], batch_size=self.batch_size, metric_attribute='test_recall')
+            self.log("test_sensitivity/F", self.test_recall[3], metric_attribute='test_recall')
+            self.log("test_sensitivity/Q", self.test_recall[4], metric_attribute='test_recall')
+            self.log("test_sensitivity/mean", (self.test_recall[0] + self.test_recall[1] + self.test_recall[2] + self.test_recall[3] + self.test_recall[4]) / 5, metric_attribute='test_recall')
+        else:
+            self.log("test_sensitivity/mean", (self.test_recall[0] + self.test_recall[1] + self.test_recall[2]) / 3, metric_attribute='test_recall')
 
         # ppv
         self.test_precision = self.test_precision.to(preds.device)
         self.test_precision(preds, targets)
-        self.log("test_ppv/N", self.test_precision[0], batch_size=self.batch_size, metric_attribute='test_precision')
-        self.log("test_ppv/S", self.test_precision[1], batch_size=self.batch_size, metric_attribute='test_precision')
-        self.log("test_ppv/V", self.test_precision[2], batch_size=self.batch_size, metric_attribute='test_precision')
+        self.log("test_ppv/N", self.test_precision[0], metric_attribute='test_precision')
+        self.log("test_ppv/S", self.test_precision[1], metric_attribute='test_precision')
+        self.log("test_ppv/V", self.test_precision[2], metric_attribute='test_precision')
         if self.num_classes == 5:
-            self.log("test_ppv/F", self.test_precision[3], batch_size=self.batch_size, metric_attribute='test_precision')
-            self.log("test_ppv/Q", self.test_precision[4], batch_size=self.batch_size, metric_attribute='test_precision')
+            self.log("test_ppv/F", self.test_precision[3], metric_attribute='test_precision')
+            self.log("test_ppv/Q", self.test_precision[4], metric_attribute='test_precision')
+            self.log("test_ppv/mean", (self.test_precision[0] + self.test_precision[1] + self.test_precision[2] + self.test_precision[3] + self.test_precision[4]) / 5, metric_attribute='test_precision')
+        else:
+            self.log("test_ppv/mean", (self.test_precision[0] + self.test_precision[1] + self.test_precision[2]) / 3, metric_attribute='test_precision')
 
         # auroc  
         self.test_auroc = self.test_auroc.to(logits.device)
         self.test_auroc(logits, targets)
-        self.log("test_auroc", self.test_auroc, batch_size=self.batch_size)
+        self.log("test_auroc", self.test_auroc)
 
         return loss_cls + loss_r_peak_pos * self.r_peaks_lambda    
     
