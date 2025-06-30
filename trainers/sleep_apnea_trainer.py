514347eb7b8e3458d73f4b0b35e9e918e769c581
luna97 <rikilnl@gmail.com>
2025-06-30 18:44:44 +0200

    wip

--- trainers/sleep_apnea_trainer.py
+++ trainers/sleep_apnea_trainer.py
@@ -217,7 +217,6 @@ class TrainingSleepApnea(CommonTrainerDownstream):
             targets = targets[:, :min_length] if targets.dim() > 1 else targets[:min_length]  
             preds = preds[:, :min_length] if preds.dim() > 1 else preds[:min_length]
 
-
             mask = (targets != -1).squeeze()
             loss_cls = nn.functional.binary_cross_entropy_with_logits(logits[mask], targets[mask], reduction='mean')
 
@@ -236,8 +235,8 @@ def format_to_segment(preds, target, patch_size, segment_size=6000):
 
     if num_patches % patches_in_segment != 0:
         # we can skip the very last part
-        preds = preds[:, :-(num_patches % patches_in_segment)]
-        target = target[:, :-(num_patches % patches_in_segment)]
+        preds = preds[:-(num_patches % patches_in_segment)] if preds.ndim == 1 else preds[:, :-num_patches % patches_in_segment]
+        target = target[:, :-(num_patches % patches_in_segment)] if target.ndim == 1 else target[:, :-num_patches % patches_in_segment]
 
     # take the prediction and group for patches_in_segment
     preds = preds.view(-1, num_segments, patches_in_segment)
