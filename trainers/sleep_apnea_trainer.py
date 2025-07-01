a4bb4dc8cb00ab38264c3dbdc0d622ac938effc7
luna97 <rikilnl@gmail.com>
2025-07-01 08:58:09 +0200

    wip

--- trainers/sleep_apnea_trainer.py
+++ trainers/sleep_apnea_trainer.py
@@ -235,8 +235,8 @@ def format_to_segment(preds, target, patch_size, segment_size=6000):
 
     if num_patches % patches_in_segment != 0:
         # we can skip the very last part
-        preds = preds[:, :-num_patches % patches_in_segment]
-        target = target[:, :-num_patches % patches_in_segment]
+        preds = preds[:, :-(num_patches % patches_in_segment)]
+        target = target[:, :-(num_patches % patches_in_segment)]
 
     # take the prediction and group for patches_in_segment
     preds = preds.view(-1, num_segments, patches_in_segment)
