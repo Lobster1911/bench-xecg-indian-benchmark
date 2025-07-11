c357a2e524bb30b2c96cb70003dc3a983f486074
luna97 <rikilnl@gmail.com>
2025-07-11 18:26:28 +0200

    wip

--- train_mit_bih.py
+++ train_mit_bih.py
@@ -55,8 +55,8 @@ def train(config, run=None, wandb=False):
         weights = None
 
     train_dataloader = DataLoader(train_dataset, batch_size=config.batch_size, shuffle=True, num_workers=config.num_workers, collate_fn=mit_bih.make_collate_fn(config))
-    val_batch_size = 1 if config.split_val_by_patient and not (config.use_transformer) else config.batch_size
-    val_num_workers = 0 if config.split_val_by_patient and not (config.use_transformer) else config.num_workers
+    val_batch_size = 1 if config.split_val_by_patient and not (config.use_transformers) else config.batch_size
+    val_num_workers = 0 if config.split_val_by_patient and not (config.use_transformers) else config.num_workers
     val_dataloader = DataLoader(val_dataset, batch_size=val_batch_size, shuffle=False, collate_fn=mit_bih.make_collate_fn(config), num_workers=val_num_workers)
 
     test_dataset = mit_bih.ECGMITBIHDataset(config, split='test', augmentations=get_transforms(config, split='test'))
