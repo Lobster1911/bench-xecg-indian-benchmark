6d3ae3264fdd95b2f474a6655639a24c36f6be1d
luna97 <rikilnl@gmail.com>
2025-07-14 18:11:23 +0200

    h100 train

---
+++ slurm_train_h100.sh
@@ -0,0 +1,19 @@
+#!/bin/bash -l
+#SBATCH --job-name=train_model
+#SBATCH --partition=h100
+#SBATCH --gpus=1
+#SBATCH --ntasks=1
+#SBATCH --cpus-per-task=128
+#SBATCH --mem-per-cpu=512M
+#SBATCH --time=7-00:00:00
+#SBATCH -o ./logs/slurm_output_%j_%x.out # STDOUT
+
+echo "Visible GPUs: "
+echo $CUDA_VISIBLE_DEVICES
+
+#source /home/$USER/.bashrc
+#conda init
+#conda activate xlstm_pretrained
+
+# run script from above
+srun ~/.conda/envs/xlstm_pretrained/bin/python -u $1
