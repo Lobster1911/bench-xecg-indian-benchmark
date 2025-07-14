09fe791b4e535fa4c98765b33a357af61d7fa39d
Angus Nicolson <angusjnicolson@gmail.com>
2025-07-14 14:57:29 +0200

    Initial DeepBeat Commit

---
+++ dataset/deepbeat.py
@@ -0,0 +1,83 @@
+
+from pathlib import Path
+
+import torch
+import yaml
+import pandas as pd
+import numpy as np
+
+from pretraining_dataset import PretrainDataset
+
+class DeepBeatDataset(PretrainDataset):
+    def __init__(self, config, split='train', global_augmentations=None, local_augmentations=None):
+        super().__init__(config, split=split, global_augmentations=global_augmentations, local_augmentations=local_augmentations)
+        self.data_folder = Path(config.data_folder_deepbeat)
+        self.signals = None
+        self.info_dict = {"fs": 32}
+
+        if split == 'train':
+            path = self.data_folder / 'train.npz'
+        elif split == 'val':
+            path = self.data_folder / 'validate.npz'
+        elif split == 'test':
+            path = self.data_folder / 'test.npz'
+        else:
+            raise ValueError(f'Unknown split {split}')
+
+        data = np.load(path, allow_pickle=True)
+        self.signals = data['signal']
+        self.qa_label = data['qa_label']
+        self.rhythm = data['rhythm']
+        self.parameters = data['parameters']
+
+    def __len__(self):
+        return len(self.signals)
+
+    def __getitem__(self, idx):
+        signal = self.signals[idx]
+        signal = self.resample_if_needed(signal, self.info_dict)
+
+        labels = self.rhythm[idx]
+
+        out = {
+            'signal': signal,
+            'label': labels,
+        }
+        return out
+
+
+class ConfigDict(dict):
+    def __getitem__(self, key):
+        return self.get(key, None)
+
+    def __getattr__(self, key):
+        return self.get(key, None)
+
+    def __setattr__(self, key, value):
+        self[key] = value
+
+    # merge the two configs, if the key is not in the config file, use the default value
+    def update(self, u):
+        for k, v in u.items():
+            if isinstance(v, dict) and isinstance(self.get(k), dict):
+                self[k].update(v)
+            else:
+                self[k] = v
+
+
+if __name__ == '__main__':
+
+    base_data_path = Path("../../../data/DeepBeat_PPG")
+    val_path = base_data_path / 'validate.npz'
+
+    config_path = "../config_defaults/train_deepbeat_config_defaults.yaml"
+    with open(config_path, 'r') as file:
+        config = yaml.safe_load(file)
+
+    config = ConfigDict(config)
+    dataset = DeepBeatDataset(config, split="val")
+
+    print(dataset[0])
+    print(dataset[0]["signal"].shape)
+    print(dataset[1]["label"].shape)
+    print("Done!")
---
+++ train_deepbeat.py
@@ -0,0 +1,79 @@
+import os
+from torch import utils
+import lightning as L
+from lightning.pytorch.loggers import WandbLogger
+from models.classification import xLSTMClassification
+
+from dataset.deepbeat import DeepBeatDataset
+import dataset.generic_utils as generic_utils
+from lightning.pytorch.callbacks import ModelCheckpoint, EarlyStopping, LearningRateMonitor
+
+from trainers.ptb_xl_trainer import TrainingPTB_XL
+import torch
+import argparse
+import os
+import numpy as np
+from tqdm import tqdm
+import utils.utils as utils
+from utils.utils import get_training_class_weights_multilabel
+from torch.utils.data import DataLoader, Dataset, ConcatDataset, Subset
+from torchvision import transforms
+from dataset.generic_utils import get_transforms
+import st_mem.encoder as encoder
+from ecg_jepa.models import load_encoder
+
+
+import argparse
+parser = argparse.ArgumentParser(description='Train a model')
+parser.add_argument('--config_file', type=str, default='configs/train_ppg_run_config.yaml', help='Path to the config file')
+
+def train(config, run=None, wandb=False):
+    # set deterministic training
+    if config.deterministic: L.seed_everything(42)
+    
+    train_dataset =  DeepBeatDataset(config, split='train', global_augmentations=get_transforms(config))
+    print(f"Train dataset size: {len(train_dataset)}")
+    val_dataset = DeepBeatDataset(config, split='val', global_augmentations=get_transforms(config, split='val'))
+    print(f"Val dataset size: {len(val_dataset)}")
+
+    if config.training_pct < 1.0:
+        train_dataset = utils.split_dataset_preserve_labels(train_dataset, split_ratio=config.training_pct)
+
+    train_dataloader = DataLoader(train_dataset, batch_size=config.batch_size, shuffle=True, num_workers=config.num_workers)
+    val_dataloader = DataLoader(val_dataset, batch_size=config.batch_size, shuffle=False, num_workers=config.num_workers)
+
+    test_dataset = DeepBeatDataset(config, split='test', global_augmentations=get_transforms(config, split='test'))
+    test_dataloader = DataLoader(test_dataset, batch_size=config.batch_size, shuffle=False, num_workers=config.num_workers)
+
+    base_model = utils.get_base_model(config)
+    
+    log_every_n_steps = max(1, len(train_dataset) // (config.batch_size * 10))
+    print(f"Logging every {log_every_n_steps} steps")
+
+    model = TrainingDeepBeat(model=base_model, config=config, len_train_dataset=len(train_dataset))
+
+    early_stopping = EarlyStopping(monitor=config.monitor_metric, check_finite=True, patience=config.patience, mode='max')
+    nan_stop = EarlyStopping(monitor='val_loss', check_finite=True, patience=config.epochs, mode='min')
+    lr_monitor = LearningRateMonitor(logging_interval='step')
+
+    if wandb:
+        checkpoint_callback = ModelCheckpoint(monitor=config.monitor_metric, mode='max')
+        prj = f'train-ptbxl-{config.classification_taksk}-{config.task}'
+        wand_logger = WandbLogger(project=prj, experiment=run, config=config)
+        wand_logger.watch(model, log='gradients')
+        trainer = L.Trainer(max_epochs=config.epochs, logger=wand_logger, callbacks=[early_stopping, lr_monitor, checkpoint_callback, nan_stop], gradient_clip_val=config.grad_clip, log_every_n_steps=log_every_n_steps)
+    else:
+        trainer = L.Trainer(logger=False, max_epochs=config.epochs, callbacks=[early_stopping, nan_stop], gradient_clip_val=config.grad_clip, log_every_n_steps=log_every_n_steps)
+
+    trainer.fit(model=model, train_dataloaders=train_dataloader, val_dataloaders=val_dataloader)
+    trainer.test(model=model, dataloaders=test_dataloader)
+
+
+# if main
+if __name__ == '__main__':
+    torch.set_float32_matmul_precision('medium')
+
+    args = parser.parse_args()
+    config = utils.parse_config(args.config_file, 'config_defaults/train_deepbeat_config_defaults.yaml')
+
+    train(config, wandb=config.wandb_log)
