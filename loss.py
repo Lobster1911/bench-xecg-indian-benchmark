22c3ca2533cd00f5fb6f8ab9a2955b345da9892e
luna97 <rikilnl@gmail.com>
2025-08-22 14:44:31 +0200

    fixed cholesky under amp

--- loss.py
+++ loss.py
@@ -88,8 +88,9 @@ class MCRLoss(nn.Module):
         scalar = p / (m * self.eps)
         I = torch.eye(p, device=cov_list.device)
         loss:torch.Tensor = 0
-        for i in range(num_views):
-            loss += torch.linalg.cholesky_ex(I + scalar * cov_list[i])[0].diagonal().log().sum()
+        with torch.amp.autocast('cuda', dtype=torch.float32):
+            for i in range(num_views):
+                loss += torch.linalg.cholesky_ex(I + scalar * cov_list[i])[0].diagonal().log().sum()
         loss /= num_views
         # loss *= (p+m)/(p*m) # the balancing factor gamma, you can also use the next line. This is ultimately a heuristic, so feel free to experiment.
         # loss *= ((self.eps * m) ** 0.5 / p)
