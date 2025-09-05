2d19e87fa64ff646a550b6bb749477e3b5ae8031
luna97 <rikilnl@gmail.com>
2025-09-05 12:21:26 +0200

    fix

--- trainers/sleep_apnea_trainer.py
+++ trainers/sleep_apnea_trainer.py
@@ -27,32 +27,6 @@ class TrainingSleepApnea(CommonTrainerDownstream):
         self.valid_auc = torchmetrics.AUROC(task='binary', ignore_index=-1)
         self.test_auc = torchmetrics.AUROC(task='binary', ignore_index=-1)
 
-        self.is_recurrent = config.is_recurrent
-
-        # if self.is_recurrent:
-        #     self.train_f1 = torchmetrics.F1Score(task='binary', ignore_index=-1)
-        #     self.valid_f1 = torchmetrics.F1Score(task='binary', ignore_index=-1)
-        #     self.test_f1 = torchmetrics.F1Score(task='binary', ignore_index=-1)
-
-        #     self.train_feature_auc = torchmetrics.AUROC(task='binary', ignore_index=-1)
-        #     self.valid_feature_auc = torchmetrics.AUROC(task='binary', ignore_index=-1)
-        #     self.test_feature_auc = torchmetrics.AUROC(task='binary', ignore_index=-1)
-
-        #     self.train_auc = torchmetrics.AUROC(task='binary', ignore_index=-1)
-        #     self.valid_auc = torchmetrics.AUROC(task='binary', ignore_index=-1)
-        #     self.test_auc = torchmetrics.AUROC(task='binary', ignore_index=-1)
-
-        #     # self.val_per_patient_metric = PerPatientMetric()
-        #     # self.test_per_patient_metric = PerPatientMetric()
-        # else: 
-        #     # Initialize metrics for validation and test
-        #     self.val_metric = ECGSegmentMetric()
-        #     self.test_metric = ECGSegmentMetric()
-
-        # self.train_feature_acc = torchmetrics.classification.accuracy.BinaryAccuracy(ignore_index=-1)
-        # self.valid_feature_acc = torchmetrics.classification.accuracy.BinaryAccuracy(ignore_index=-1)
-        # self.test_feature_acc = torchmetrics.classification.accuracy.BinaryAccuracy(ignore_index=-1)
-
     def training_step(self, batch, _):
         loss, logits, preds, targets = self.predict_batch(batch)
 
@@ -68,24 +42,6 @@ class TrainingSleepApnea(CommonTrainerDownstream):
         train_auc = self.train_auc.to(preds.device)
         train_auc(logits, targets)
         self.log('train_auc', train_auc, prog_bar=False)
-
-        # if self.is_recurrent:
-        #     preds_segment, targets_segment, logits_segment = format_to_segment(preds, targets, self.patch_size)
-        #     train_acc = self.train_acc.to(preds.device)
-        #     train_acc(preds_segment, targets_segment)
-        #     self.log('train_acc', train_acc, prog_bar=True)
-
-        #     train_f1 = self.train_f1.to(preds.device)
-        #     train_f1(preds_segment, targets_segment)
-        #     self.log('train_f1', train_f1, prog_bar=True)
-
-        #     train_feature_auc = self.train_feature_auc.to(preds.device)
-        #     train_feature_auc(logits, targets)
-        #     self.log('train_feature_auc', train_feature_auc, prog_bar=False)
-
-        #     train_auc = self.train_auc.to(preds.device)
-        #     train_auc(logits_segment, targets_segment)
-        #     self.log('train_auc', train_auc, prog_bar=False)
 
         return loss
     
@@ -105,29 +61,6 @@ class TrainingSleepApnea(CommonTrainerDownstream):
         val_auc(logits, targets)
         self.log('val_auc', val_auc, prog_bar=True)
 
-        # if self.is_recurrent:
-            # preds_segment, targets_segment, logits_segment = format_to_segment(preds, targets, self.patch_size)
-            # valid_acc = self.valid_acc.to(preds.device)
-            # valid_acc(preds_segment, targets_segment)
-            # self.log('val_acc', valid_acc, prog_bar=True)
-
-            # valid_f1 = self.valid_f1.to(preds.device)
-            # valid_f1(preds_segment, targets_segment)
-            # self.log('val_f1', valid_f1, prog_bar=True)
-
-            # valid_feature_auc = self.valid_feature_auc.to(preds.device)
-            # valid_feature_auc(logits, targets)
-            # self.log('val_feature_auc', valid_feature_auc, prog_bar=False)
-
-            # valid_auc = self.valid_auc.to(preds.device)
-            # valid_auc(logits_segment, targets_segment)
-            # self.log('val_auc', valid_auc, prog_bar=False)
-
-            # self.val_per_patient_metric.update(preds_segment, targets_segment, seg_ids)
-        # else:
-        #     # Update the segment metric with current batch
-        #     self.val_metric.update(preds, targets, seg_ids)
-
         return loss
             
     def test_step(self, batch, _):
@@ -142,80 +75,16 @@ class TrainingSleepApnea(CommonTrainerDownstream):
         test_f1(preds, targets)
         self.log('test_f1', test_f1, prog_bar=False)
 
-        # test_feature_f1 = self.test_feature_f1.to(preds.device)
-        # test_feature_f1(preds, targets)
-        # self.log('test_feature_f1', test_feature_f1, prog_bar=False)
-
         test_auc = self.test_auc.to(preds.device)
         test_auc(logits, targets)
         self.log('test_auc', test_auc, prog_bar=False)
 
-        # if self.is_recurrent:
-        #     preds_segment, targets_segment, logits_segment = format_to_segment(preds, targets, self.patch_size)
-        #     test_acc = self.test_acc.to(preds.device)
-        #     test_acc(preds_segment, targets_segment)   
-        #     self.log('test_acc', test_acc, prog_bar=False)
-            
-        #     test_f1 = self.test_f1.to(preds.device)
-        #     test_f1(preds_segment, targets_segment)
-        #     self.log('test_f1', test_f1, prog_bar=False)
-
-        #     test_feature_auc = self.test_feature_auc.to(preds.device)
-        #     test_feature_auc(logits, targets)
-        #     self.log('test_feature_auc', test_feature_auc, prog_bar=False)
-
-        #     test_auc = self.test_auc.to(preds.device)
-        #     test_auc(logits_segment, targets_segment)
-        #     self.log('test_auc', test_auc, prog_bar=False)
-
-        #     self.test_per_patient_metric.update(preds_segment, targets_segment, seg_ids)
-        # else:
-        #     # Update the segment metric with current batch
-        #     self.test_metric.update(preds, targets, seg_ids)
- 
         return loss
     
-    def on_validation_epoch_end(self):
-        # Compute and log metrics
-        # if not self.is_recurrent:
-        #     metrics = self.val_metric.compute()
-        #     self.log_dict({
-        #         'val_auc': metrics['auc_macro'],
-        #         'val_f1': metrics['f1_macro'],
-        #         'val_acc': metrics['accuracy_micro'],
-        #         'val_patient_acc': metrics['patient_acc'],
-        #         'val_patient_f1': metrics['patient_f1']
-        #     })
-        # else:
-        #     metrics = self.val_per_patient_metric.compute()
-        #     self.log_dict({
-        #         'val_patient_acc': metrics['patient_acc'],
-        #         'val_patient_f1': metrics['patient_f1']
-        #     })
-        super().on_validation_epoch_end()
-        
-    def on_test_epoch_end(self):
-        # Compute and log final test metrics
-        # if not self.is_recurrent:
-        #     metrics = self.test_metric.compute()
-        #     self.log_dict({
-        #         'test_auc': metrics['auc_macro'],
-        #         'test_f1': metrics['f1_macro'],
-        #         'test_acc': metrics['accuracy_micro'],
-        #         'test_patient_acc': metrics['patient_acc'],
-        #         'test_patient_f1': metrics['patient_f1']
-        #     })
-        # else:
-        #     metrics = self.test_per_patient_metric.compute()
-        #     self.log_dict({
-        #         'test_patient_acc': metrics['patient_acc'],
-        #         'test_patient_f1': metrics['patient_f1']
-        #     })
-        super().on_test_epoch_end()
             
     def predict_batch(self, batch):
         x = batch["signals"]
-        targets = batch['labels']
+        targets = batch['labels'].unsqueeze(-1)
 
         # segment_ids = batch['segment_ids']  # patient ids are not used in the training, but we keep them for consistency
 
@@ -230,18 +99,7 @@ class TrainingSleepApnea(CommonTrainerDownstream):
 
         # print(f'targets shape: {targets.shape}, logits shape: {logits.shape}, preds shape: {preds.shape}, mask shape: {mask.shape}')
 
-        #if not self.is_recurrent:
-        # targets = targets.squeeze(-1)
         loss_cls = nn.functional.binary_cross_entropy_with_logits(logits[mask], targets[mask], reduction='mean')
-        # else:
-        #     targets = targets.repeat_interleave(logits.shape[-1] // targets.shape[-1], dim=-1)  # repeat targets for binary classification
-        #     min_length = min(logits.shape[-1], targets.shape[-1])
-        #     logits = logits[:, :min_length] 
-        #     targets = targets[:, :min_length] 
-        #     preds = preds[:, :min_length] 
-
-        #     loss_cls = nn.functional.binary_cross_entropy_with_logits(logits[mask], targets[mask], reduction='mean')
-
         return loss_cls, logits, preds, targets.long()
 
 
