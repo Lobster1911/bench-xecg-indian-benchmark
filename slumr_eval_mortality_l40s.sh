971fd6be39b5c0b88ff4a88ba93995d8a9da7351
luna97 <rikilnl@gmail.com>
2025-09-10 11:19:38 +0200

    wip

--- slumr_eval_mortality_l40s.sh
+++ slumr_eval_mortality_l40s.sh
@@ -3,7 +3,7 @@
 #SBATCH --partition=l40s
 #SBATCH --gpus=1
 #SBATCH --ntasks=1
-#SBATCH --cpus-per-task=64
+#SBATCH --cpus-per-task=32
 #SBATCH --mem-per-cpu=512M
 #SBATCH --time=7-00:00:00
 #SBATCH -o ./logs/slurm_output_%j_%x.out # STDOUT
@@ -21,4 +21,4 @@ ulimit -n 16384
 ulimit -n
 
 # run script from above
-srun ~/.conda/envs/xlstm_pretrained/bin/python -u $1 --run_ids $2 $3 $4 $5 $6 --batch_size $7 --max_length_signal $8 --data_folder_music $9 --num_workers 64
+srun ~/.conda/envs/xlstm_pretrained/bin/python -u $1 --run_ids $2 $3 $4 $5 $6 --batch_size $7 --max_length_signal $8 --data_folder_music $9 --num_workers 32
